f1191193e48463050710586aa3769f5af4cbb22c
Raphael Häfele <raphael.haefele@gmx.at>
2024-12-01 20:10:16 +0100

    feat(Day02): init and preparation

---
+++ Day_02/Program.cs
@@ -0,0 +1,10 @@
+﻿using ResourceReader = AdventOfCodeLibrary.ResourceReader;
+
+ResourceReader resourceReader = new();
+
+string rawData = resourceReader.GetResource(2);
+
+Console.WriteLine("Solution Part 1:" );
+Console.WriteLine("Solution Part 2:" );
+
+Console.ReadLine();
